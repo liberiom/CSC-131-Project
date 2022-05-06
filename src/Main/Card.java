2c66666e8f76efd2aff06b254a8b5333f56a10fc
liberiom <liberiomukdani@csus.edu>
2022-05-05 21:45:12 -0700

    Card testing

---
+++ src/Main/Card.java
@@ -0,0 +1,49 @@
+package Main;
+
+import Data.Vector2D;
+
+public class Card {
+	private int number = -100;
+	private Vector2D coords;
+	private boolean isShowingNumber;
+	
+	public Card(int number, Vector2D coords) {
+		this.number = number;
+		this.coords = coords;
+		this.isShowingNumber = false;
+	}
+	
+	public Card() { // Alternative Constructor
+		
+	}
+	
+	public Card(Vector2D coords) {
+		this.coords = coords;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public Vector2D getCoords() {
+		return coords;
+	}
+
+	public boolean isShowingNumber() {
+		return isShowingNumber;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public void setCoords(Vector2D coords) {
+		this.coords = coords;
+	}
+
+	public void setShowingNumber(boolean isShowingNumber) {
+		this.isShowingNumber = isShowingNumber;
+	}
+	
+	
+}
